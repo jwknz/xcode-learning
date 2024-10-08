13270481e285b1bbf036b870e5a1f8e438ff62cf
Jeff Kranenburg <jwkranenburg@icloud.com>
2024-10-08 20:31:45 +1300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Jeff Kranenburg on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningTests/LearningTests.swift
@@ -0,0 +1,16 @@
+//
+//  LearningTests.swift
+//  LearningTests
+//
+//  Created by Jeff Kranenburg on 08/10/2024.
+//
+
+import Testing
+
+struct LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
